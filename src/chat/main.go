722ca5dbda776f61db39b778112b0a7ae0877f43
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-05-21 10:31:28 +0200

    Command line chat working

--- src/chat/main.go
+++ src/chat/main.go
@@ -1,6 +1,11 @@
 package chat
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
 
 var (
 	host = flag.String("h", "localhost", "host")
@@ -15,4 +20,25 @@ var (
 
 func Run() {
 	flag.Parse()
+	connData := fmt.Sprintf("%s:%d", *host, *port)
+	listener, err := net.Listen("tcp", connData)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Listening", connData)
+
+	go Broadcast()
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go HandleConnection(conn)
+	}
 }
--- src/main.go
+++ src/main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"flag"
 	"go-platzi-advanced-dp-conc/src/chat"
-	"go-platzi-advanced-dp-conc/src/netcat"
 )
 
-var app = flag.String("app", "chat", "Select chat or netcat app to run")
-
 func main() {
-	flag.Parse()
-
-	switch *app {
-	case "netcat":
-		netcat.Run()
-	default:
-		chat.Run()
-	}
+	chat.Run()
 }
